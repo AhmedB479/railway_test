43b8c7ce247d48610b62c95bf4317fc87328932c
Ahmed B. <ahmedbashaar974@gmail.com>
2024-08-29 00:54:55 +0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -29,6 +29,14 @@ def index():
 load_dotenv()
 
 # Models for API input
+# Models for API input
+class URLInput(BaseModel):
+    url: str
+
+class QuestionInput(BaseModel):
+    docs: List[Document]
+    question: str
+
 def url_loader(user_input):
     loader = WebBaseLoader(user_input)
     docs = loader.load()
@@ -106,24 +114,19 @@ def gemini(retriever, question):
     return rag_chain.invoke(question)
 
 # FastAPI Endpoints
-@app.post("/extract/{URLInput}")
-def extract_information(URLInput: str):
-    url = []
-    url.append(URLInput)
-    docs = url_loader(url[0])
-    print(docs)
+@app.post("/extract/")
+def extract_information(input: URLInput):
+    docs = url_loader(input.url)
     result = extractor(docs)
-    print(result)
     return {"extracted_information": result}
 
-# @app.post("/ask-question/{QuestionInput}")
-# def ask_question(QuestionInput: str):
+# @app.post("/ask-question/")
+# def ask_question(input: QuestionInput):
 #     try:
-#         gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-#         docs = url_loader(QuestionInput)
-#         retriever = pine_index(docs, gemini_embeddings)
-#         answer = gemini(retriever, QuestionInput)
-#         return {"answer": answer}
+        # gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        # retriever = pine_index(input.docs, gemini_embeddings)
+        # answer = gemini(retriever, input.question)
+        # return {"answer": answer}
 #     except Exception as e:
 #         raise HTTPException(status_code=500, detail=str(e))
 
